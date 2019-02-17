7d2703392d91b6ec7857de66845da9041706593d
n04x <ThomasBacks@gmail.com>
2019-02-17 11:36:18 -0500

    Redo maplayout
    Change the map-layout to simplify node calculation and size.

--- Assets/Scripts/TileBaseNode.cs
+++ Assets/Scripts/TileBaseNode.cs
@@ -7,7 +7,7 @@ public class TileBaseNode : MonoBehaviour
     // create enumerator for neighbours (up to 8)
     public enum Neighbours
     {
-        up = 0, up_right = 1, right = 2, down_right = 3, down = 4, down_left = 5, left = 6, up_left = 7
+        upper = 0, upper_right = 1, right = 2, lower_right = 3, lower = 4, lower_left = 5, left = 6, upper_left = 7
     };
     public TileBaseNode[] neighbours = new TileBaseNode[8];
 
@@ -15,6 +15,8 @@ public class TileBaseNode : MonoBehaviour
     float cost_so_far;
     float heuristic_value;
     float total_estimated_value;
+
+    public TileBaseNode previous;
 
     void Start() {
 
--- Assets/Scripts/TileGraph.cs
+++ Assets/Scripts/TileGraph.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class TileGraph : MonoBehaviour
 {
+    public AIBehaviour penguin_script;
     public TileBaseNode tile_based_node_script;
     public Vector3 map_size;
     public float radius;
@@ -22,15 +23,26 @@ public class TileGraph : MonoBehaviour
     public List<TileBaseNode> tile_path_list = new List<TileBaseNode>();
     public List<TileBaseNode> tile_open_list = new List<TileBaseNode>();
     public List<TileBaseNode> tile_closed_list = new List<TileBaseNode>();
-    public TileBaseNode end_tile_node;
-    public List<List<float>> tile_clister_lookup = new List<List<float>>();
+    public List<TileBaseNode> room1_tile_nodes = new List<TileBaseNode>();
+    public List<TileBaseNode> room2_tile_nodes = new List<TileBaseNode>();
+    public List<TileBaseNode> room3_tile_nodes = new List<TileBaseNode>();
+
+    public List<List<float>> tile_cluster_lookup = new List<List<float>>();
+    public TileBaseNode target_tile_node;
+
     public bool tile_base_mode = true;
 
     public TileBaseNode random_tile_node;
 
+    int room;
+    int another_room;
+
+    public enum Modes { DIJSKTRA, EUCLIDEAN, CLUSTER };
+    public Modes current_mode = Modes.DIJSKTRA;
+
     void Start()
     {
-        map_size = new Vector3(150, 0, 60);
+        map_size = new Vector3(148, 0, 60);
         tile_size = new Vector3(map_size.x / tile_size_density / tile_size_density, 0, map_size.z / tile_size_density);
         radius = tile_size.x / 2;
 
@@ -38,7 +50,49 @@ public class TileGraph : MonoBehaviour
         CreateTile();
         ClearTile();
         FindStartNode();
-        FindEndNode();
+        //FindEndNode(room);
+
+    }
+
+    void Update()
+    {
+        int counter = 0;
+        if(tile_base_mode)
+        {
+            foreach(TileBaseNode node in tile_base_node_list)
+            {
+                node.NodeVisible();
+                //node.GetComponent<Renderer>().material.color = Color.white;
+            }
+            // unvisited node.
+            foreach(TileBaseNode node in tile_open_list)
+            {
+                node.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            // closed node.
+            foreach(TileBaseNode node in tile_closed_list)
+            {
+                node.GetComponent<Renderer>().material.color = Color.grey;
+            }
+            // selected path for our penguin.
+            foreach(TileBaseNode node in tile_path_list)
+            {
+                node.GetComponent<Renderer>().material.color = Color.green;
+            }
+            // starting and ending point.
+            start_node.GetComponent<Renderer>().material.color = Color.red;
+            end_node.GetComponent<Renderer>().material.color = Color.red;
+
+            // if the penguin is on the pathfinding, continue its movement.
+            if(tile_path_list.Count > counter && end_node == tile_path_list[tile_path_list.Count - 1])
+            {
+                target_tile_node.transform.position = end_node.transform.position;
+                if(Vector3.Angle(penguin_script.transform.forward, (tile_path_list[counter].transform.position - penguin_script.transform.position)) > 50)
+                {
+                    
+                }
+            }
+        }
     }
 
     void CreateTile()
@@ -58,14 +112,14 @@ public class TileGraph : MonoBehaviour
 
     void CreateNeighbours(TileBaseNode node)
     {
-        node.neighbours[(int)TileBaseNode.Neighbours.up] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, tile_size.z)));
-        node.neighbours[(int)TileBaseNode.Neighbours.up_right] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(0, 0, tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.upper] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(0, 0, tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.upper_right] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(tile_size.x, 0, tile_size.z)));
         node.neighbours[(int)TileBaseNode.Neighbours.right] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(tile_size.x, 0, 0)));
-        node.neighbours[(int)TileBaseNode.Neighbours.down_right] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(tile_size.x, 0, -tile_size.z)));
-        node.neighbours[(int)TileBaseNode.Neighbours.down] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(0, 0, -tile_size.z)));
-        node.neighbours[(int)TileBaseNode.Neighbours.down_left] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, -tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.lower_right] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(tile_size.x, 0, -tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.lower] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(0, 0, -tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.lower_left] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, -tile_size.z)));
         node.neighbours[(int)TileBaseNode.Neighbours.left] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, 0)));
-        node.neighbours[(int)TileBaseNode.Neighbours.up_left] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, tile_size.z)));
+        node.neighbours[(int)TileBaseNode.Neighbours.upper_left] = tile_base_node_list.Find(n => (n.transform.position - node.transform.position) == (new Vector3(-tile_size.x, 0, tile_size.z)));
     }
 
     void ClearTile()
@@ -82,4 +136,24 @@ public class TileGraph : MonoBehaviour
     void FindStartNode() {
         // find the starting according to my penguin position
     }
+
+    private void FindEndNode(int room)
+    {
+        while(room == another_room)
+        {
+            another_room = UnityEngine.Random.Range(0, 3);
+        }
+        if(another_room == 0)
+        {
+            target_tile_node = room1_tile_nodes[UnityEngine.Random.Range(0, room2_tile_nodes.Count)];
+        }
+        else if(another_room == 1)
+        {
+            target_tile_node = room2_tile_nodes[UnityEngine.Random.Range(0, room2_tile_nodes.Count)];
+        } else
+        {
+            target_tile_node = room3_tile_nodes[UnityEngine.Random.Range(0, room3_tile_nodes.Count)];
+        }
+    }
+
 }
